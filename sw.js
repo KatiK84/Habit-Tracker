9ca5b7d62676aba5d419653c1cf548a047c34bd2
KatiK84 <e.kokoreva@outlet-salzmann.de>
2026-01-14 23:46:34 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="habit-cache-v1";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{const r=e.request;e.respondWith(caches.match(r).then(cached=>cached||fetch(r).then(resp=>{if(r.method==="GET"&&new URL(r.url).origin===location.origin){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(r,copy)).catch(()=>{})}return resp}).catch(()=>cached)))});
